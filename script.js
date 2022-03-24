89cddba609a2c1a7587ee5d2dea0fcf8202c1d35
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-03-24 12:36:38 +1000

    added RGB and Black functionality

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 
 const screenDiv = document.querySelector(".div-screen"); //select div-screen
 
-
 function getSize(){
     let size = prompt("Please enter a number of rows", 16);
     if (size <= 100){
-    createGrid(size);
+        createGrid(size);
     } else {
         alert("please enter a number between 1 & 100");
         getSize();
@@ -15,47 +14,49 @@ function getSize(){
 
 window.onload = getSize();
 
-
 function createGrid(size){
-   
     let sizeOfSquare = (size) => 960 / size; //determines size of each child div & puts value in sizeOfSquare
 
     for (let i = 0; i< (size*size); i++){
         const newDiv = document.createElement("div");
         newDiv.classList.add("square");
         screenDiv.style.cssText = `grid-template-columns: repeat(${size}, 1fr)`;
-        
         newDiv.style.height = sizeOfSquare(size); 
         newDiv.style.width = sizeOfSquare(size);
         screenDiv.appendChild(newDiv);
     }
 }
 
-    //  ON HOVER
-    screenDiv.addEventListener('mousemove', function(e){
-        e.target.style.background = 'rgba(0,0,0,0.8)';
-    });
+screenDiv.addEventListener('mouseover', function(e){
+    e.target.style.background = "rgba(0,0,0,0.8)";
+});
 
+const btnClear = document.querySelector(".btn-clear"); 
+const secControls = document.querySelector(".sec-controls"); //select section with  buttons
 
-// BUTTON
-const btnClear = document.querySelector(".btn-clear");
+secControls.addEventListener('click', function(e){ //add event listener to section
 
-
-
-
-btnClear.addEventListener('click', function(){
-
-    if (btnClear.textContent == "Clear"){
+    if (e.target.textContent == "RGB"){ // if clicked on RGB 
+        screenDiv.addEventListener('mouseover', function(e){
+            let r = Math.floor(Math.random() * (255 - 0 + 1));
+            let g = Math.floor(Math.random() * (255 - 0 + 1));
+            let b = Math.floor(Math.random() * (255 - 0 + 1));
+            e.target.style.background = `rgba(${r},${g},${b},0.8)`;
+        });
+    } else if (e.target.textContent == "Black"){ // if clicked on Black 
+        screenDiv.addEventListener('mouseover', function(e){
+            e.target.style.background = "rgba(0,0,0,0.8)";
+        });
+    } else if (e.target.textContent == "Clear"){  // if clicked on Clear
         const sqDivs = document.querySelectorAll(".square");
         for (let i=0; i<sqDivs.length;i++){
             screenDiv.removeChild(sqDivs[i]);
-    }
-    btnClear.textContent = "Create New Grid";
-} else {
+        }
+        btnClear.textContent = "Create New Grid";
+    } else {
     btnClear.textContent = "Clear"
     getSize();
-}
-
+    }
 });
 
 
